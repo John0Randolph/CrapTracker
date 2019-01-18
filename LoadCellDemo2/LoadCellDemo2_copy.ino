b0f0283bebb2a239d753beb956448181f15cf3b7
John0Randolph <john_randolph@brown.edu>
2019-01-17 23:52:39 -0500

    outline added

--- LoadCellDemo2/LoadCellDemo2_copy.ino
+++ LoadCellDemo2/LoadCellDemo2_copy.ino
@@ -5,17 +5,22 @@
 
 HX711 scale(DOUT, CLK);
 
+// float of how much the scale is naturally off by
 float calibrateVal;
+// array for storing recent data over time
+// array for averages of data over longer amount of time
 
 // note: a pound is about 600000-605000
 // verfied with 2lbs, 2.5lbs
+// seems to fritz out greater than that (i.e. 5 pounds was off)
 
 /**
  * Next steps:
  * Go to BDW, design better scale
  * Calibrate for pounds / grams
  * Write code that takes a difference and records weight
- * 
+ */
+
 
 void setup() {
   Serial.begin(9600);
@@ -35,6 +40,13 @@ void calibrate() {
 
 long val = 0;
 float count = 0;
+
+/**
+ * TODO:
+ * add functionality that
+ * - saves recent data to the array
+ * - calls concatData
+ */
 
 void loop() {
   count = count + 1;
@@ -56,3 +68,29 @@ void loop() {
   }
 
 }
+
+
+/**
+ * TODO:
+ * function that concatenates data so that we have averages over time
+ * this data will be used to determine longer time weight changes
+ */
+
+/**
+ * TODO:
+ * function that looks at averages over time and calculates 
+ * differences to get the weight of the discharge
+ */
+
+/**
+ * TODO:
+ * function that checks when someone has sat down
+ */
+
+/**
+ * TODO:
+ * function that checks when someone has stood up
+ * calculate weight of discharge
+ * if incalculatable, print that message
+ * if not, push to leaderboard
+ */
